78580a8723a19b796f05808fcb333e390b5fbeaa
zarif007 <zarifhuq786@gmail.com>
2024-12-15 11:24:42 +0600

    fix: undefined handling in optional chaining

--- src/components/NodeHeader.tsx
+++ src/components/NodeHeader.tsx
@@ -7,41 +7,49 @@ import { useReactFlow } from "@xyflow/react";
 import { CreateFlowNode } from "@/lib/workflow/createFlowNode";
 import { ICustomNode } from "@/types/customNode";
 
-const NodeHeader = ({ taskType, nodeId }: { taskType: taskType, nodeId: string }) => {
+const NodeHeader = ({
+  taskType,
+  nodeId,
+}: {
+  taskType: taskType;
+  nodeId: string;
+}) => {
   const task = taskRegistry[taskType];
   const { deleteElements, getNode, addNodes } = useReactFlow();
 
   const deleteNode = () => {
     deleteElements({
-      nodes: [{id: nodeId}]
-    })
-  }
+      nodes: [{ id: nodeId }],
+    });
+  };
 
   const copyNode = () => {
     const node = getNode(nodeId) as ICustomNode;
     const newX = node?.position.x;
-    const newY = node?.position.y + node.measured?.height! + 20;
+    const newY = node?.position.y + (node.measured?.height ?? 0) + 20;
 
     const newNode = CreateFlowNode(node?.data.type, {
       x: newX,
-      y: newY
-    })
+      y: newY,
+    });
 
-    addNodes([newNode])
-  }
+    addNodes([newNode]);
+  };
   return (
     <div className="flex items-center gap-2 p-2 justify-between bg-slate-900">
       <div className="flex items-center w-full">
         <p className="text-xs font-bold uppercase">{task.label}</p>
       </div>
-      {!task.isEntryPoint && <div className="flex space-x-1">
-        <Button variant={"ghost"} size={"icon"} onClick={deleteNode}>
-          <TrashIcon size={12} />
-        </Button>
-        <Button variant={"ghost"} size={"icon"} onClick={copyNode}>
-          <CopyIcon size={12} />
-        </Button>
-      </div>}
+      {!task.isEntryPoint && (
+        <div className="flex space-x-1">
+          <Button variant={"ghost"} size={"icon"} onClick={deleteNode}>
+            <TrashIcon size={12} />
+          </Button>
+          <Button variant={"ghost"} size={"icon"} onClick={copyNode}>
+            <CopyIcon size={12} />
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
